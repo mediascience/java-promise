22cef06619e29af4af7e81dbebb377eabc94794f
Greg Wiley <aztec.rex@jammm.com>
2014-10-27 10:43:46 -0700

    flatMap concurrency tests

--- src/test/java/fn/com/msiops/ground/promise/ConcurrencyTest.java
+++ src/test/java/fn/com/msiops/ground/promise/ConcurrencyTest.java
@@ -22,6 +22,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 import org.junit.Test;
 
@@ -85,6 +86,119 @@ public class ConcurrencyTest {
                         start.await();
                         p.on(Throwable.class, x -> {
                             if (expected.equals(x)) {
+                                emitted.incrementAndGet();
+                            }
+                        });
+                    } catch (final InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    } finally {
+                        end.countDown();
+                    }
+                }
+            });
+        }
+
+        start.countDown();
+        try {
+            end.await();
+            assertEquals(breadsz * 2, emitted.get());
+        } catch (final InterruptedException e) {
+            Thread.currentThread().interrupt();
+            fail("did not finish");
+        } finally {
+            exec.shutdownNow();
+        }
+
+    }
+
+    @Test
+    public void testManyFlatMaps() {
+
+        final int breadsz = 50;
+
+        final ExecutorService exec = Executors.newCachedThreadPool();
+        final CountDownLatch start = new CountDownLatch(1);
+        final CountDownLatch end = new CountDownLatch(breadsz * 4 + 1);
+
+        final String expected = "HI";
+        final Async<Integer> a = new Async<>();
+        final Promise<Integer> p = a.promise();
+
+        final AtomicInteger emitted = new AtomicInteger();
+
+        final Function<Object, Promise<String>> mf = new Function<Object, Promise<String>>() {
+            @Override
+            public Promise<String> apply(final Object t) {
+                final Async<String> inner = new Async<>();
+                exec.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(100);
+                            inner.succeed(expected);
+                        } catch (final InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        } finally {
+                            end.countDown();
+                        }
+                    }
+                });
+                return inner.promise();
+            }
+        };
+
+        for (int i = 0; i < breadsz; i = i + 1) {
+            exec.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        start.await();
+                        /*
+                         * should be able to use mf directly but eclipse won't
+                         * let me. so doing this hacky workaround. Note that if
+                         * use mf as the argument directly, javac has no trouble
+                         * with it
+                         */
+                        p.flatMap(hack -> mf.apply(hack)).forEach(o -> {
+                            if (expected.equals(o)) {
+                                emitted.incrementAndGet();
+                            }
+                        });
+                    } catch (final InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    } finally {
+                        end.countDown();
+                    }
+                }
+            });
+        }
+        exec.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    start.await();
+                    a.succeed(12);
+                } catch (final InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    end.countDown();
+                }
+            }
+        });
+        for (int i = 0; i < breadsz; i = i + 1) {
+            exec.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        start.await();
+                        /*
+                         * should be able to use mf directly but eclipse won't
+                         * let me. so doing this hacky workaround. Note that if
+                         * use mf as the argument directly, javac has no trouble
+                         * with it
+                         */
+                        p.flatMap(hack -> mf.apply(hack)).forEach(o -> {
+                            if (expected.equals(o)) {
                                 emitted.incrementAndGet();
                             }
                         });
